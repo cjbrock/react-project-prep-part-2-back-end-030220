46fb7bd7882e737e845084a8c7cdd5e61829ef21
cjbrock <corinna@flatironschool.com>
2020-07-20 11:22:49 -0400

    ad post study group code for friday

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Pet.create([{
+  name: "Buzz",
+  age: 7,
+  species: "Great Pyrenees/Panda"
+},
+{
+  name: "Woody",
+  age: 7,
+  species: "American Red Pitbull/Bunny"
+},
+{
+  name: "Bambi",
+  age: 2,
+  species: "HellCat" 
+},
+{
+  name: "Flower",
+  age: 2,
+  species: "RegularCat"
+},
+{
+name: "Danish",
+age: 12,
+species: "Best dog ever"
+},
+{
+name: "Misti",
+age: 13,
+species: "Blue Merle Collie"
+},
+{
+name: "Butterscotch",
+age: 14,
+species: "Sheltie"
+}])
